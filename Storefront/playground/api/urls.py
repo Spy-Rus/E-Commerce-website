67d4255c1ed2aa17eccb3cc3d68cb1a9ebd2a8e5
Spy-Rus <sp9097013@gmail.com>
2026-03-07 16:39:36 +0530

    Update urls.py

--- Storefront/playground/api/urls.py
+++ Storefront/playground/api/urls.py
@@ -1,14 +1,25 @@
 from django.urls import path
 from .api_views import (
     ProductListAPI,
-    MyOrdersAPI,
-    create_order_api,
-    pay_order_api,
+    ProductDetailAPI,
+    CartAPI,
+    CartItemAPI,
+    cart_increase,
+    cart_decrease,
+    seller_update_order_item_status_api,
 )
 
 urlpatterns = [
-    path('products/', ProductListAPI.as_view()),   # GET
-    path('orders/', create_order_api),              # POST ✅
-    path('my-orders/', MyOrdersAPI.as_view()),      # GET
-    path('pay/', pay_order_api),
+    # Public
+    path('products/', ProductListAPI.as_view()),
+    path('products/<int:id>/', ProductDetailAPI.as_view()),
+
+    # Cart
+    path('cart/', CartAPI.as_view()),
+    path('cart/<int:id>/', CartItemAPI.as_view()),
+    path('cart/<int:id>/increase/', cart_increase),
+    path('cart/<int:id>/decrease/', cart_decrease),
+
+    # Seller
+    path('seller/order-item/<int:item_id>/status/', seller_update_order_item_status_api),
 ]
